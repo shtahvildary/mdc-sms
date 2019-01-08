42442622eb15b8fccf5cd227084e03dddb3d6d6e
shtahvildary <shtahvildary@gmail.com>
2019-01-08 15:20:33 +0330

    working on port

--- smsServer.1.js
+++ smsServer.1.js
@@ -1,25 +1,29 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
+// var chokidar = require('chokidar');
 
 
+// chokidar.watch(
+//     '.', { ignored: /node_modules|\.git/,
+//         // './dir', {
+//     ignored: /^\./,
+//     persistent: true,
+//     // followSymlinks: false,
+//     // useFsEvents: false,
+//     // usePolling: false
+//   }).on('all', function(event, path) {
+//     console.log(event, path);
+//   })
+// //   .on('add', function (path) {
 
+// //     console.log('File', path, 'has been added');
 
-var chokidar = require('chokidar');
+// // })
+//   .on('ready', function() {
+//     console.log('Ready');
+//   })
+//   //.on('raw', console.log.bind(console, 'RawEvent:'))
 var modem = require("sms-gsm");
 
-/* Change to port */
 var m1 = new modem.Modem_text("/dev/tty.usbserial-A600aL36");
-
-var watcher = chokidar.watch('./dir', {ignored: /^\./, persistent: true});
-
-watcher
-  .on('add', function(path) {console.log('File', path, 'has been added');})
-
-
-
-//https://github.com/agathauy/sms-gsm
 
 /* To always re-open port in case of disconnect */
 var open_port = setInterval(() => {
@@ -32,83 +36,23 @@ var open_port = setInterval(() => {
             console.log(status);
         }
     });
-}, 3000);
+}, 8000);
 
-m1.eventEmitter.on('new message', (num, text) => {
-    console.log("New message:");
-    console.log(num);
-    console.log(text);
-
-    var msg = text.trim().split(/\s+/);
-    if (msg.toUpperCase() == "HELLO") {
-        var reply = "Hi";
-        m1.sendMessage(num, reply, (err, res) => {
+require('chokidar').watch('./dir', {
+    ignored: /node_modules|\.git/,
+    persistent: true,
+    // followSymlinks: false,
+    // useFsEvents: false,
+    // usePolling: false
+  }).on('add', function(event, path) {
+    console.log(event, path);
+    m1.sendMsg(00989122005639, "hi", (err, res) => {
             if (err) {
                 console.log(err);
             } else {
-
+                console.log("message has sent!")
             }
         });
-    }
-});
-
-
-// http.createServer(function (req, res) {
-//   console.log(`${req.method} ${req.url}`);
-//   // parse URL
-//   const parsedUrl = url.parse(req.url);
-//   var folder;
-//   var fileIndex=req.url;
-//   fileIndex=fileIndex.slice(fileIndex.lastIndexOf(".")+1)
-//   // extract URL path
-//   // let pathname = __dirname + "/files/videos" +parsedUrl.pathname;
-//   // let pathname = __dirname + "/files/photos" +parsedUrl.pathname;
-
-//   // let pathname = `.${parsedUrl.pathname}`;
-
-//   // maps file extention to MIME types
-//   const mimeType = {
-//     '.ico': 'image/x-icon',
-//     '.png': 'image/png',
-//     '.jpg': 'image/jpeg',
-    
-//     '.wav': 'audio/wav',
-//     '.mp3': 'audio/mpeg',
-    
-//     '.pdf': 'application/pdf',
-//     '.doc': 'application/msword',
-    
-//     '.ogg':'video/ogg',
-//     '.mp4':'video/mp4'
-//   };
-  
-//   let pathname = "./file" +parsedUrl.pathname;
-//   // let pathname = __dirname + "/files" +parsedUrl.pathname;
-  
-//   fs.exists(pathname, function (exist) {
-//     if(!exist) {
-//       // if the file is not found, return 404
-//       res.statusCode = 404;
-//       res.end(`File ${pathname} not found!`);
-//       return;
-//     }
-//     // // if is a directory, then look for index.html
-//     // if (fs.statSync(pathname).isDirectory()) {
-//     //   pathname += '/index.html';
-//     // }
-//     // read file from file system
-//     fs.readFile(pathname, function(err, data){
-//       if(err){
-//         res.statusCode = 500;
-//         res.end(`Error getting the file: ${err}.`);
-//       } else {
-//         // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-//         const ext = path.parse(pathname).ext;
-//         // if the file is found, set Content-type and send data
-//         res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
-//         res.end(data);
-//       }
-//     });
-//   });
-// }).listen(parseInt(port));
-// console.log(`Download Server listening on port ${port}`);
+  }).on('ready', function() {
+    console.log('Ready');
+  })
